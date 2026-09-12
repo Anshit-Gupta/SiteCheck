aca2e59ea0938b4b9d083b4d1afcbff869edcd73
Anshit-Gupta <anshitgupta777@gmail.com>
2026-09-12 22:23:09 +0530

    implemeted  concurency

--- sitecheck.go
+++ sitecheck.go
@@ -1,49 +1,63 @@
 package main
 
+//here's a list of sites, tell me which ones are up and how fast
+
 import (
-	"errors"
-	"fmt"
-	"net/http"
-	"time"
+	 // "errors"   //error handling
+	"fmt"      //formating , input/output
+	"net/http" //for http call
+	"sync" 
+	"time" //to cal latency
 )
 
 type SiteResults struct {
 	URL        string
 	StatusCode int
-	LatencyMs  time.Duration
+	LatencyMs  time.Duration //sice time.since(startTime) returns this way
+	error      string
 }
 
-func checkURL(url string) (SiteResults, error) {
-
+func checkURL(url string , wg *sync.WaitGroup , results chan SiteResults) {
+    defer wg.Done() 
 	startTime := time.Now()
-	res, err := http.Get(url) //it returns respone,error
+	res, err := http.Get(url) //it returns respone,error in error type and not string 
 	latency := time.Since(startTime)
 	if err != nil {
-		fmt.Println("something is wrong")
 
-		return SiteResults{}, errors.New("error opening the url")
+		 results <- SiteResults{ URL: url , error : err.Error()}
 	} else {
 		defer res.Body.Close() //closing the connection
-		results := SiteResults{URL: url, StatusCode: res.StatusCode, LatencyMs: latency}
-		return results, nil
+		
+		results <- SiteResults{URL: url, StatusCode: res.StatusCode, LatencyMs: latency , error: "none"} //send value into the channel 
+		
 	}
 
 }
 
-func (s SiteResults) Summary()  {
-	fmt.Printf(" Results for %s \n StatusCode : %d \n Lantency %v", s.URL, s.StatusCode, s.LatencyMs)
+func (s SiteResults) Summary() {
+	fmt.Printf(" Results for %s \n StatusCode : %d \n Lantency : %v \n error : %v \n", s.URL, s.StatusCode, s.LatencyMs , s.error)
 }
 
 func main() {
-	 var url string
-	 fmt.Println("Welcome to siteCheck!!!!")
-	 fmt.Printf("Enter the url : ")
-	 fmt.Scanln(&url)
-	result, err := checkURL(url)
-
-	if err != nil {
-		fmt.Println("somthing went wrong", err)
-	} else {
-		result.Summary()
+    var wg sync.WaitGroup
+	results := make(chan SiteResults) //create the channel- a pipe carriying ints
+	fmt.Println("Welcome to siteCheck!!!!")
+	urls := []string{"https://anshitgupta.com", "https://instagram.com", "https://lol.com", "https://google.com"}
+	start := time.Now()
+	wg.Add(len(urls))
+	for _, url := range urls {
+		 go checkURL(url , &wg ,results)
 	}
+
+	for i:=1;i<=len(urls);i++{
+		value := <- results //receive one value from the channel 
+		value.Summary()
+	}
+
+
+   
+	totalTime := time.Since(start)
+
+	fmt.Printf("Total time : %v ", totalTime)
+
 }
